6d6ee12be886c5441d5a076f948ff52217c09d5b
Sajan Adhikari <hariyoban0@gmail.com>
2023-06-29 16:00:53 +0530

    Changes Before anything

--- app/src/main/java/com/medical/medcoach/Fragment/ProfileFragment.java
+++ app/src/main/java/com/medical/medcoach/Fragment/ProfileFragment.java
@@ -2,35 +2,27 @@ package com.medical.medcoach.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
+import androidx.fragment.app.Fragment;
+
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.medical.medcoach.LoginRegisterActivity;
 import com.medical.medcoach.R;
-import com.medical.medcoach.getOTPActivity;
-
-import java.util.concurrent.TimeUnit;
 
 public class ProfileFragment extends Fragment {
 
     DatabaseReference databaseReference;
     TextView UserName;
+    Button logoutBtn;
 
 
     @Override
@@ -39,32 +31,35 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         UserName=view.findViewById(R.id.profile_name);
+        logoutBtn=view.findViewById(R.id.logout);
 
         databaseReference= FirebaseDatabase.getInstance().getReference("Users");
-        databaseReference.child("+919999996262").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()){
-                    if (task.getResult().exists()){
-                        DataSnapshot dataSnapshot = task.getResult();
-                        String Name = String.valueOf(dataSnapshot.child("Full Name").getValue());
-                        UserName.setText(Name);
+        databaseReference.child("+919999996262").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                if (task.getResult().exists()) {
+                    DataSnapshot dataSnapshot = task.getResult();
+                    String Name = String.valueOf(dataSnapshot.child("Full Name").getValue());
+                    UserName.setText(Name);
 
-                    }else {
-                        Toast.makeText(getActivity(), "User Not Exist", Toast.LENGTH_SHORT).show();
-                    }
-                }else {
-                    Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "User Not Exist", Toast.LENGTH_SHORT).show();
                 }
-
+            } else {
+                Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show();
             }
+
         });
 
-     return view;
+        logoutBtn.setOnClickListener(v->{
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getActivity(), LoginRegisterActivity.class));
+            if(getActivity()!=null)
+                getActivity().finish();
+        });
+
+        return view;
     }
-    public void logout(View view) {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getActivity(), LoginRegisterActivity.class));
-        getActivity().finish();
-    }
+
+
+
 }
